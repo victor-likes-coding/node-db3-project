14ddf0558ad7beaf99e1e781064159b68eac6c7e
Victor Tran <victor-likes-coding@tempestas.io>
2023-03-25 00:45:53 -0400

    write the middlewares

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,5 @@
+const { findById } = require('./scheme-model');
+
 /*
   If `scheme_id` does not exist in the database:
 
@@ -6,9 +8,16 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
-
-}
+const checkSchemeId = async (req, res, next) => {
+  const { id } = req.params;
+  const scheme = await findById(id);
+  if (!scheme) {
+    next({
+      status: 404,
+      message: `scheme with scheme_id ${id} not found`,
+    });
+  }
+};
 
 /*
   If `scheme_name` is missing, empty string or not a string:
@@ -18,9 +27,17 @@ const checkSchemeId = (req, res, next) => {
     "message": "invalid scheme_name"
   }
 */
-const validateScheme = (req, res, next) => {
-
-}
+const validateScheme = async (req, res, next) => {
+  const { scheme_name } = req.body;
+  if (!scheme_name || typeof scheme_name !== 'string') {
+    next({
+      status: 400,
+      message: 'invalid scheme_name',
+    });
+    return;
+  }
+  next();
+};
 
 /*
   If `instructions` is missing, empty string or not a string, or
@@ -32,11 +49,19 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
-
-}
+  const { instructions, step_number } = req.body;
+  if (!instructions || typeof instructions !== 'string' || !step_number || typeof step_number !== 'number' || step_number < 1) {
+    next({
+      status: 400,
+      message: 'invalid step',
+    });
+    return;
+  }
+  next();
+};
 
 module.exports = {
   checkSchemeId,
   validateScheme,
   validateStep,
-}
+};
